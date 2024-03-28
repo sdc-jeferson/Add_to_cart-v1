b781dd96438bd9c95b143982409a7ca382cf2d73
sdc-jeferson <jeferson.contato.df@gmail.com>
2024-03-27 21:54:18 -0300

    feat : separados variaveis neste arquivo

--- elements.js
+++ elements.js
@@ -3,11 +3,10 @@ const cartModal = document.getElementById("modal-cart");
 const cartFooter = document.getElementById("cart-footer-btn");
 const closeModalCart = document.getElementById("close-modal");
 const cartCount = document.querySelector(".cart-count");
-
+const cartContainer = document.getElementById("item-cart");
 const finallyOrder = document.getElementById("finally-order-btn");
 const adressInput = document.getElementById("address-input");
 const inputWarnMessage = document.getElementById("input-warn");
-const cartItemsContainer = document.getElementById("cart-items");
 const totalPayment = document.getElementById("tota-items");
 
 export {
@@ -19,6 +18,6 @@ export {
   finallyOrder,
   adressInput,
   inputWarnMessage,
-  cartItemsContainer,
   totalPayment,
+  cartContainer,
 };
